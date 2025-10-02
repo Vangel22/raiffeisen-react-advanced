bd58004b0e3a3979270bc4966a7ff7a2c704665c
Vangel22 <h.vangel22@gmail.com>
2025-10-02 13:06:15 +0200

    save

--- next_project/src/app/users/page.tsx
+++ next_project/src/app/users/page.tsx
@@ -1,5 +1,5 @@
-const getServerData = () => {
-  return fetch("https://rickandmortyapi.com/api/character")
+const getServerData = async () => {
+  return fetch("https://dummyjson.com/users")
     .then((res) => res.json())
     .then((data) => data);
 };
